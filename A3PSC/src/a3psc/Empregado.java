f21f2ea7abca4030e11a7ea7c7240eca50232678
Charamelle <bibi@damianifx.com.br>
2023-06-10 17:10:19 -0300

    inicio - pessoas

--- A3PSC/src/a3psc/Empregado.java
+++ A3PSC/src/a3psc/Empregado.java
@@ -9,8 +9,16 @@ package a3psc;
 public class Empregado extends Pessoa{
     
     /* --------- ATRIBUTOS --------- */
+    private String idEmpregado;
+    private String usuario;
+    private String senha;
+    private static int qtdeEmpregado;
 
-     /* ------- MÉTODOS CONSTRUTORES ------- */
-
-     /* -------- OUTROS MÉTODOS -------- */
+    /* ------- MÉTODOS CONSTRUTORES ------- */
+    public Empregado(String nome, long cpf,String dataNascimento, String endereco, long telefone, String usuario, String senha){
+        super(nome, cpf, dataNascimento, endereco, telefone);
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    /* -------- OUTROS MÉTODOS -------- */
 }
--- A3PSC/src/a3psc/Pessoa.java
+++ A3PSC/src/a3psc/Pessoa.java
@@ -4,13 +4,18 @@ public class Pessoa {
     /* --------- ATRIBUTOS --------- */
     private String nome;
     private long cpf;
-    private int[]dataNascimento;
     private String endereco;
     private long telefone;
 
     /* ------- MÉTODOS CONSTRUTORES ------- */
 
+    public Pessoa(String nome, long cpf,String dataNascimento, String endereco, long telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
     /* -------- OUTROS MÉTODOS -------- */
      
-    
 }
