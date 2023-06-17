e380b1f4d4779d8c14cc52d04379ced9214baa9a
Charamelle <bibi@damianifx.com.br>
2023-06-17 14:05:03 -0300

    Revert "Revert "coisas da DB""
    This reverts commit 17e49ac8c0a81e090cbfa067b1b944a0fecd7a93.

--- A3PSC/src/a3psc/A3PSC.java
+++ A3PSC/src/a3psc/A3PSC.java
@@ -1,33 +1,9 @@
 package a3psc;
 
-import java.util.Scanner;
-
 public class A3PSC {
 
     
     public static void main(String[] args) {
         
-        //int idProduto, String nomeProduto, int quantidadeProduto 
-        Scanner sc = new Scanner(System.in);
-        Estoque Estoque = new Estoque();
-        
-        // testes
-            
-        Estoque.addProduto("kiss", 1, "preta");
-        System.out.println(Estoque.produtos[1][0].getSetorProduto());
-        Estoque.getFromID("pos", "1#1#1#1");
-        //Estoque.editarEstampa("1.1.1.1", "cu");
-            /*for (int i=0;i<3;i++){
-            System.out.println("setor:");
-            String setor = sc.nextLine();
-
-            System.out.println("cor:");
-            String cor = sc.nextLine();
-            
-
-            Estoque.addProduto(setor, cor);
-            }*/
-            
-
     } 
 }
