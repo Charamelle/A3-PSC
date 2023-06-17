17e49ac8c0a81e090cbfa067b1b944a0fecd7a93
Charamelle <bibi@damianifx.com.br>
2023-06-17 14:04:31 -0300

    Revert "coisas da DB"
    This reverts commit 9b7ecfdbfb8740fb34d4356537f25dc150e76254.

---
+++ A3PSC/src/a3psc/Pessoa.java
@@ -0,0 +1,21 @@
+package a3psc;
+
+public class Pessoa {
+    /* --------- ATRIBUTOS --------- */
+    private String nome;
+    private long cpf;
+    private String endereco;
+    private long telefone;
+
+    /* ------- MÉTODOS CONSTRUTORES ------- */
+
+    public Pessoa(String nome, long cpf,String dataNascimento, String endereco, long telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    /* -------- OUTROS MÉTODOS -------- */
+     
+}
